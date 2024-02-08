a173137ac7b33eedaa22b8502b39d1ff0193c0ef
tsa96 <tom@trumpetdust.org>
2024-02-08 08:25:36 +0000

    Update color palettes

--- colors/src/palettes.js
+++ colors/src/palettes.js
@@ -17,23 +17,6 @@ export default [
     ]
   },
   {
-    // Blues, riffing of our signature #1896D3 which is right in the middle
-    paletteName: 'blue',
-    swatches: [
-      { name: '50', color: 'DCEFFF' },
-      { name: '100', color: 'BDE4FF' },
-      { name: '200', color: '9AD9FB' },
-      { name: '300', color: '75CAF5' },
-      { name: '400', color: '4CB6E9' },
-      { name: '500', color: '1896D3' },
-      { name: '600', color: '127AC5' },
-      { name: '700', color: '0E60B6' },
-      { name: '800', color: '1A438A' },
-      { name: '900', color: '182954' },
-      { name: '950', color: '121B33' }
-    ]
-  },
-  {
     paletteName: 'pale',
     swatches: [
       { name: '50', color: 'B9DEFF' },
@@ -50,19 +33,132 @@ export default [
     ]
   },
   {
+    // Blues, riffing of our signature #1896D3 which is right in the middle
+    paletteName: 'blue',
+    swatches: [
+      { name: '50', color: 'E7F4FF' },
+      { name: '100', color: 'C5E7FF' },
+      { name: '200', color: 'A4DDFB' },
+      { name: '300', color: '75CAF5' },
+      { name: '400', color: '4CB6E9' },
+      { name: '500', color: '1896D3' },
+      { name: '600', color: '106EB1' },
+      { name: '700', color: '0A4888' },
+      { name: '800', color: '122E5E' },
+      { name: '900', color: '0F1B37' },
+      { name: '950', color: '0A0F1C' }
+    ]
+  },
+  // Below swatches are all using tints.dev, lightness mode
+  {
+    paletteName: 'purple',
+    swatches: [
+      { name: '50', color: 'F3EBFE' },
+      { name: '100', color: 'EADDFD' },
+      { name: '200', color: 'D2B6FC' },
+      { name: '300', color: 'BD94FA' },
+      { name: '400', color: 'A871F9' },
+      { name: '500', color: '914CF7' },
+      { name: '600', color: '6B10F4' },
+      { name: '700', color: '4F08B9' },
+      { name: '800', color: '34057A' },
+      { name: '900', color: '1B033F' },
+      { name: '950', color: '0D011D' }
+    ]
+  },
+  {
+    paletteName: 'pink',
+    swatches: [
+      { name: '50', color: 'FFF5F9' },
+      { name: '100', color: 'FFEBF3' },
+      { name: '200', color: 'FED7E7' },
+      { name: '300', color: 'FEC3DB' },
+      { name: '400', color: 'FDAFCE' },
+      { name: '500', color: 'FD9DC4' },
+      { name: '600', color: 'FB4B92' },
+      { name: '700', color: 'F00563' },
+      { name: '800', color: 'A00342' },
+      { name: '900', color: '500221' },
+      { name: '950', color: '280110' }
+    ]
+  },
+  {
+    paletteName: 'red',
+    swatches: [
+      { name: '50', color: 'F9EBEC' },
+      { name: '100', color: 'F4D7D8' },
+      { name: '200', color: 'EAB3B5' },
+      { name: '300', color: 'DF8B8E' },
+      { name: '400', color: 'D5676B' },
+      { name: '500', color: 'CA4146' },
+      { name: '600', color: 'A82F33' },
+      { name: '700', color: '7C2225' },
+      { name: '800', color: '541719' },
+      { name: '900', color: '280B0C' },
+      { name: '950', color: '140606' }
+    ]
+  },
+  {
+    paletteName: 'orange',
+    swatches: [
+      { name: '50', color: 'FDF4E7' },
+      { name: '100', color: 'FBE8D0' },
+      { name: '200', color: 'F7D4A6' },
+      { name: '300', color: 'F3BD77' },
+      { name: '400', color: 'F0A94C' },
+      { name: '500', color: 'EC931F' },
+      { name: '600', color: 'C57711' },
+      { name: '700', color: '92580C' },
+      { name: '800', color: '633C08' },
+      { name: '900', color: '2F1C04' },
+      { name: '950', color: '180E02' }
+    ]
+  },
+  {
+    paletteName: 'yellow',
+    swatches: [
+      { name: '50', color: 'FDF8E7' },
+      { name: '100', color: 'FCF3D4' },
+      { name: '200', color: 'F9E7A9' },
+      { name: '300', color: 'F7DA7E' },
+      { name: '400', color: 'F4CE53' },
+      { name: '500', color: 'F1C325' },
+      { name: '600', color: 'D3A50E' },
+      { name: '700', color: '9E7C0A' },
+      { name: '800', color: '695207' },
+      { name: '900', color: '352903' },
+      { name: '950', color: '181302' }
+    ]
+  },
+  {
+    paletteName: 'green',
+    swatches: [
+      { name: '50', color: 'EFF6E9' },
+      { name: '100', color: 'E0EED3' },
+      { name: '200', color: 'C3DEAB' },
+      { name: '300', color: 'A3CC7F' },
+      { name: '400', color: '84BB53' },
+      { name: '500', color: '67983C' },
+      { name: '600', color: '527930' },
+      { name: '700', color: '3E5B24' },
+      { name: '800', color: '2A3E19' },
+      { name: '900', color: '141D0C' },
+      { name: '950', color: '0A0F06' }
+    ]
+  },
+  {
     paletteName: 'tier',
     swatches: [
-      { name: '1', color: '9EFF40' },
-      { name: '2', color: '49F94C' },
-      { name: '3', color: '42EAF8' },
-      { name: '4', color: '4282F8' },
-      { name: '5', color: '583EF2' },
-      { name: '6', color: '8538F2' },
-      { name: '7', color: 'FFA904' },
-      { name: '8', color: 'F8400F' },
-      { name: '9', color: 'E72929' },
-      { name: '10', color: '950000' }
+      { name: '1', color: '28ABEA' },
+      { name: '2', color: '2E85E5' },
+      { name: '3', color: '335AD3' },
+      { name: '4', color: '4A30CE' },
+      { name: '5', color: '5F0AB7' },
+      { name: '6', color: 'AB1669' },
+      { name: '7', color: '970237' },
+      { name: '8', color: '940303' },
+      { name: '9', color: '7E0202' },
+      { name: '10', color: '630101' }
     ]
   }
-  // TODO: Worth doing full palettes for green, red, maybe orange
 ];
